f024bbcce612f2edf84df278c7fe0fe4d7bfd707
akitsu-sanae <akitsu.sanae@gmail.com>
2017-02-24 13:45:25 +0900

    DIMACS format

--- sat.ml
+++ sat.ml
@@ -3,12 +3,27 @@ module Truth = Set.Make (struct
     let compare = compare
 end)
 
-let lines = Std.input_list stdin;;
+let rec read_n_variables () =
+    let line = read_line () in
+    if line.[0] = 'p' then
+        (* p cnf <n variables> <n clauses> *)
+        int_of_string @@ List.nth (Str.split (Str.regexp "[ \t]+") line) 3
+    else
+        read_n_variables ()
 
-let clauses = List.map
-    (fun line ->
-        List.map int_of_string @@ Str.split (Str.regexp "[ \t]+") line)
-    lines
+let _ = read_n_variables ()
+
+let read_data () = begin
+    let lines = Std.input_list stdin in
+    let line_to_variables line =
+        let variable_strs = Str.split (Str.regexp "[ \t]") line in
+        let variables = List.rev_map int_of_string variable_strs in
+        List.tl variables (* first element is 0 *)
+    in
+    List.rev_map line_to_variables lines
+end
+
+let clauses = read_data ()
 
 exception Unsat
 
@@ -31,7 +46,14 @@ let rec solve clauses truth =
             let new_truth = solve_impl clause truth Truth.empty in
             solve clauses @@ Truth.union truth new_truth;;
 
-let answer = solve clauses Truth.empty;;
-Truth.iter (fun n -> print_string @@ string_of_int n ^ ", ") answer;;
-print_newline ()
+try
+    let answer = solve clauses Truth.empty in
+    print_endline "s SATISFIABLE";
+    print_string "v ";
+    Truth.iter (fun n -> print_string @@ string_of_int n ^ " ") answer;
+    print_endline " 0"
+with Unsat ->
+    print_endline "s UNSATISFIABLE";
+    exit 20
+;;
 
