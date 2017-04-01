4fc8b7684acb0fd8a43e97ede650177be04ea47d
akitsu-sanae <akitsu.sanae@gmail.com>
2017-04-01 10:22:05 +0900

    add Readme and LICENSE

---
+++ README.md
@@ -0,0 +1,17 @@
+# SAT
+
+SAT is my toy sat solver.
+
+sat reads DIMACS cnf from `stdin`.  
+
+# build
+please run `omake`.  
+
+sat depends on `extlib` and `str` library.
+
+# Copyright
+Copyright (C) 2017 akitsu sanae.  
+Distributed under the Boost Software License, Version 1.0. 
+(See accompanying file LICENSE or copy at http://www.boost/org/LICENSE_1_0.txt)  
+
+
--- sat.ml
+++ sat.ml
@@ -1,3 +1,10 @@
+(*============================================================================
+  Copyright (C) 2017 akitsu sanae
+  https://github.com/akitsu-sanae/sat
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*)
+
 module Clause = Set.Make (struct
     type t = int
     let compare = compare
