2a11fdd829144ef264fbbdd478554a83fa7b01af
akitsu-sanae <akitsu.sanae@gmail.com>
2017-04-01 21:33:20 +0900

    use set of clause, not list of one

--- sat.ml
+++ sat.ml
@@ -10,6 +10,11 @@ module Clause = Set.Make (struct
     let compare = compare
 end)
 
+module Problem = Set.Make (struct
+    type t = Clause.t
+    let compare = fun lhs rhs -> Clause.cardinal lhs - Clause.cardinal rhs
+end)
+
 let rec read_n_variables () =
     let line = read_line () in
     if line.[0] = 'p' then
@@ -20,50 +25,55 @@ let rec read_n_variables () =
 
 let _ = read_n_variables ()
 
-let read_data ()  : Clause.t list =
+let read_data () =
     let lines = Std.input_list stdin in
-    let line_to_variables line =
+    let line_to_clause line =
         let variable_strs = Str.split (Str.regexp "[ \t]+") line in
         let variables = List.rev_map int_of_string variable_strs in
         Clause.of_list @@ List.tl variables (* first element is 0 *)
     in
-    List.rev_map line_to_variables lines
+    Problem.of_list @@ List.rev_map line_to_clause lines
 
 let (>>) f g = function x -> f(g(x))
 
-let clauses = read_data ()
+let problem = read_data ()
 
-let print_clauses clauses =
+let print_problem problem =
     let print_clause clause =
         print_endline @@ Clause.fold (fun i acc ->  acc ^ string_of_int i ^ ", ") clause ""
     in
-    List.iter print_clause clauses
+    Problem.iter print_clause problem
 
 let print_result result =
     print_endline @@ List.fold_left (fun acc i -> acc ^ string_of_int i ^ " " ) "" result ^ "0"
 
+
+(* Clause  : Set of int    *)
+(* Problem : Set of Clause *)
+
 exception Unsat
 
-let rec subst (clauses: Clause.t list) given =
-    let clauses = List.filter (fun clause -> not @@ Clause.mem given clause) clauses in
-    let clauses = List.rev_map (fun clause -> Clause.remove (-given) clause) clauses in
-    if List.for_all (not >> Clause.is_empty) clauses then clauses
+let rec subst problem given =
+    let problem = Problem.filter (fun clause -> not @@ Clause.mem given clause) problem in
+    let problem = Problem.map (fun clause -> Clause.remove (-given) clause) problem in
+    if Problem.for_all (not >> Clause.is_empty) problem then problem
     else raise Unsat
 
-let rec solve (clauses: Clause.t list) result : int list =
-    let piv = Clause.choose @@ List.hd clauses in
+let rec solve problem result =
+    let piv = Clause.choose @@ Problem.choose problem in
     try
-        let clauses = subst clauses piv in
-        if List.length clauses = 0 then piv::result
-        else solve clauses (piv::result)
+        let problem = subst problem piv in
+        if Problem.is_empty problem then piv::result
+        else solve problem (piv::result)
     with Unsat ->
-        let clauses = subst clauses (-piv) in
-        if List.length clauses = 0 then (-piv)::result
-        else solve clauses ((-piv)::result)
+        let problem = subst problem (-piv) in
+        if Problem.is_empty problem then (-piv)::result
+        else solve problem ((-piv)::result)
 
 let _ =
     try
-        let result = solve clauses [] in
+        let result = solve problem [] in
+        print_problem problem;
         print_endline "SAT";
         print_result @@ List.sort (fun l r -> abs l - abs r) result
     with Unsat ->
